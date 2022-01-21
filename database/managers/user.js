cf84d06f397bd717b08f100c6e2c862d4afdbb96
DeFaNy <69439212+DeFaNy@users.noreply.github.com>
2022-01-21 14:00:58 +0300

    Update

--- database/managers/user.js
+++ database/managers/user.js
@@ -3,11 +3,11 @@ const { Users } = require('../models')
 const user = {
 
     get: (id) => Users.findOne({ id }).lean(),
-    create: (id, refferalValue) => {
+    create: (id, referralValue) => {
 
         const newUser = new Users({
             id,
-            'ref.refferer': refferalValue,
+            'ref.refferer': referralValue ? referralValue : 0 ,
         })
 
         newUser.save().then()
